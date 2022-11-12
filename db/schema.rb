0423afe071a15fb2c3cdc3afe61dc6b4e00d4e6b
johncjejen <john@john-VirtualBox>
2022-11-12 13:17:21 -0500

    validate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_10_151651) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -24,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_151651) do
   end
 
   create_table "borrow_books", force: :cascade do |t|
-    t.integer "book_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
     t.integer "copy_number"
     t.date "borrow_date"
     t.date "due_date"
@@ -33,14 +36,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_10_151651) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "copy_book_id"
+    t.bigint "copy_book_id"
     t.index ["book_id"], name: "index_borrow_books_on_book_id"
     t.index ["copy_book_id"], name: "index_borrow_books_on_copy_book_id"
     t.index ["user_id"], name: "index_borrow_books_on_user_id"
   end
 
   create_table "copy_books", force: :cascade do |t|
-    t.integer "books_id", null: false
+    t.bigint "books_id", null: false
     t.integer "copy_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
